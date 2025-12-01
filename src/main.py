2f98b5a61645d4d0406940d3724eb098202952a2
Ahmed <ahmedtarekelbahgy@gmail.com>
2025-12-01 09:51:22 +0200

    update readme location

--- src/main.py
+++ src/main.py
@@ -4,15 +4,17 @@ from controller.main_controller import MainController
 
 PASSWORD = "2002"
 
+
 def main():
-    ctk.set_appearance_mode('dark')
-    ctk.set_default_color_theme('blue')
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
     root = ctk.CTk()
-    root.title('Elbahgy لادارة المخازن')
-    root._state_before_windows_set_titlebar_color = 'zoomed'
-    
+    root.title("Elbahgy لادارة المخازن")
+    root._state_before_windows_set_titlebar_color = "zoomed"
+
     app = MainController(root)
     root.mainloop()
 
-if __name__ == '__main__':    
+
+if __name__ == "__main__":
     main()
